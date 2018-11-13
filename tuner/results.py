a72bdaf5cb13e4ac9a5227a05466197949bf2f19
Brandon Navra <brandon.navra@gmail.com>
2018-11-13 21:18:54 +1100

    Add ability to save results as CSV

--- tuner/results.py
+++ tuner/results.py
@@ -1,3 +1,4 @@
+import csv
 import shutil
 from pathlib import Path
 from re import Match
@@ -26,6 +27,19 @@ class ResultsRecorder:
 
     def record_result(self, *args, **kwargs):
         raise NotImplementedError
+
+    @staticmethod
+    def extract_results(results: Match):
+        if results is not None:
+            depth, seldepth, nps = (
+                results.group("depth"),
+                results.group("seldepth"),
+                results.group("nps"),
+            )
+        else:
+            depth, seldepth, nps = None, None, None
+
+        return depth, seldepth, nps
 
     def complete_run(self, config_file):
         shutil.move(str(config_file.absolute()), config_file.parent / "processed")
@@ -81,16 +95,9 @@ class XLSXResultsRecorder(ResultsRecorder):
         self.wb.save(filename=self.environment.results_path.name)
 
     def record_result(self, results: Match, config_file: Path):
-        if results is not None:
-            depth, seldepth, nps = (
-                results.group("depth"),
-                results.group("seldepth"),
-                results.group("nps"),
-            )
-        else:
-            depth, seldepth, nps = None, None, None
-
+        depth, seldepth, nps = self.extract_results(results)
         config_filename = str(config_file.name)
+
         row = int(config_filename.split(".")[0])
         self.ws.cell(row + 2, 1, config_filename)
         self.ws.cell(row + 2, 2, int(nps if nps else 0))
@@ -98,4 +105,32 @@ class XLSXResultsRecorder(ResultsRecorder):
         self.ws.cell(row + 2, 4, int(seldepth if seldepth else 0))
 
         self.save_workbook()
+        self.complete_run(config_file)
+
+
+@register("csv")
+class CSVResultsRecorder(ResultsRecorder):
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+
+        file_path = str(self.environment.results_path.absolute())
+        self.file = open(file_path, "w")
+        self.writer = csv.DictWriter(
+            self.file, fieldnames=("Filename", "NPS", "DEPTH", "SELDEPTH")
+        )
+        self.writer.writeheader()
+
+    def __del__(self):
+        self.file.close()
+
+    def record_result(self, results: Match, config_file: Path):
+        depth, seldepth, nps = self.extract_results(results)
+
+        row = {
+            "Filename": str(config_file.name),
+            "NPS": nps,
+            "DEPTH": depth,
+            "SELDEPTH": seldepth,
+        }
+        self.writer.writerow(row)
         self.complete_run(config_file)
