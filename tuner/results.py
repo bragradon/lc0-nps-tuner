3c698d975a282f21d26f99c334b0f994b71f30f3
Brandon Navra <brandon.navra@gmail.com>
2020-02-08 11:29:29 +1100

    Change results file to contain the runtime options per result

--- tuner/results.py
+++ tuner/results.py
@@ -44,6 +44,11 @@ class ResultsRecorder:
     def complete_run(self, config_file):
         shutil.move(str(config_file.absolute()), config_file.parent / "processed")
 
+    def read_run_config(self, config_file):
+        with open(config_file) as config:
+            config = config.read().replace('--', '').strip().split('\n')
+            return dict(sorted(i.split('=', 1) for i in config))
+
 
 @register("xlsx")
 class XLSXResultsRecorder(ResultsRecorder):
@@ -70,26 +75,30 @@ class XLSXResultsRecorder(ResultsRecorder):
         return wb, ws
 
     def add_results_workbook_headers(self, worksheet):
-        worksheet["A1"] = "LC0 Parameters Run Results"
-        worksheet["A2"] = "Filename"
-        worksheet["B2"] = "NPS"
-        worksheet["C2"] = "DEPTH"
-        worksheet["D2"] = "SELDEPTH"
+        worksheet["A1"] = "Best NPS"
+        worksheet["B1"] = "Filename"
+        worksheet["A2"] = "=MAX(E:E)"
+        worksheet["B2"] = '=INDIRECT("D" & MATCH(MAX(E:E), E:E, 0))'
 
-        worksheet["F2"] = "Best NPS"
-        worksheet["G2"] = "Filename"
-        worksheet["F3"] = "=MAX(B:B)"
-        worksheet["G3"] = '=INDIRECT("A" & MATCH(MAX(B:B), B:B, 0))'
+        worksheet["A4"] = "Best Depth"
+        worksheet["B4"] = "Filename"
+        worksheet["A5"] = "=MAX(F:F)"
+        worksheet["B5"] = '=INDIRECT("D" & MATCH(MAX(F:F), F:F, 0))'
 
-        worksheet["F5"] = "Best Depth"
-        worksheet["G5"] = "Filename"
-        worksheet["F6"] = "=MAX(C:C)"
-        worksheet["G6"] = '=INDIRECT("A" & MATCH(MAX(C:C), C:C, 0))'
+        worksheet["A7"] = "Best SelDepth"
+        worksheet["B7"] = "Filename"
+        worksheet["A8"] = "=MAX(G:G)"
+        worksheet["B8"] = '=INDIRECT("D" & MATCH(MAX(G:G), G:G, 0))'
 
-        worksheet["F8"] = "Best SelDepth"
-        worksheet["G8"] = "Filename"
-        worksheet["F9"] = "=MAX(D:D)"
-        worksheet["G9"] = '=INDIRECT("A" & MATCH(MAX(D:D), D:D, 0))'
+        worksheet["D1"] = "LC0 Parameters Run Results"
+        worksheet["D2"] = "Filename"
+        worksheet["E2"] = "NPS"
+        worksheet["F2"] = "DEPTH"
+        worksheet["G2"] = "SELDEPTH"
+
+        for i, header in enumerate(sorted(self.environment.options['options'].keys())):
+            cellref = worksheet.cell(row=2, column=i + ord("h") - ord("a") + 1)
+            cellref.value = header
 
     def save_workbook(self):
         self.wb.save(filename=str(self.environment.results_path.absolute()))
@@ -97,12 +106,16 @@ class XLSXResultsRecorder(ResultsRecorder):
     def record_result(self, results: Match, config_file: Path):
         depth, seldepth, nps = self.extract_results(results)
         config_filename = str(config_file.name)
+        config = self.read_run_config(config_file)
 
         row = int(config_filename.split(".")[0])
-        self.ws.cell(row + 2, 1, config_filename)
-        self.ws.cell(row + 2, 2, int(nps if nps else 0))
-        self.ws.cell(row + 2, 3, int(depth if depth else 0))
-        self.ws.cell(row + 2, 4, int(seldepth if seldepth else 0))
+        self.ws.cell(row + 2, ord("d") - ord("a") + 1, config_filename)
+        self.ws.cell(row + 2, ord("e") - ord("a") + 1, int(nps if nps else 0))
+        self.ws.cell(row + 2, ord("f") - ord("a") + 1, int(depth if depth else 0))
+        self.ws.cell(row + 2, ord("g") - ord("a") + 1, int(seldepth if seldepth else 0))
+
+        for i, (name, value) in enumerate(config.items()):
+            self.ws.cell(row + 2, ord("h") - ord("a") + 1 + i, value)
 
         self.save_workbook()
         self.complete_run(config_file)
@@ -114,9 +127,13 @@ class CSVResultsRecorder(ResultsRecorder):
         super().__init__(environment)
 
         file_path = str(self.environment.results_path.absolute())
+
+        header = ["Filename", "NPS", "DEPTH", "SELDEPTH"]
+        header += sorted(self.environment.options['options'].keys())
+
         self.file = open(file_path, "w")
         self.writer = csv.DictWriter(
-            self.file, fieldnames=("Filename", "NPS", "DEPTH", "SELDEPTH")
+            self.file, fieldnames=header
         )
         self.writer.writeheader()
 
@@ -125,12 +142,14 @@ class CSVResultsRecorder(ResultsRecorder):
 
     def record_result(self, results: Match, config_file: Path):
         depth, seldepth, nps = self.extract_results(results)
-
+        config = self.read_run_config(config_file)
+        
         row = {
             "Filename": str(config_file.name),
             "NPS": nps,
             "DEPTH": depth,
             "SELDEPTH": seldepth,
+            **config,
         }
         self.writer.writerow(row)
         self.complete_run(config_file)
